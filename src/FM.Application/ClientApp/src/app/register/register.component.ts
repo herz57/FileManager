5b384934f2a0d866985017a281b5aca5bb65ecdd
valerii <exet52hl@gmail.com>
2020-01-08 14:38:42 +0200

    [client] added sign in, sign up validation

--- src/FM.Application/ClientApp/src/app/register/register.component.ts
+++ src/FM.Application/ClientApp/src/app/register/register.component.ts
@@ -6,7 +6,7 @@ import { UserService } from '../services/user.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['../shared/styles/loginlogoutform.scss', './register.component.scss']
 })
 export class RegisterComponent implements OnInit {
 
